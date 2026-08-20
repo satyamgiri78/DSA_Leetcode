1f3d6c0935747fe6bd38f4679858a440ab1b7d22
satyamgiri78 <satyamgiri9569@gmail.com>
2026-08-20 23:14:13 +0530

    Time: 5 ms (90.28%), Space: 47 MB (55.76%) - LeetHub

--- 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.java
+++ 2486-append-characters-to-string-to-make-subsequence/2486-append-characters-to-string-to-make-subsequence.java
@@ -8,13 +8,14 @@ class Solution {
 
         while((i<m) && (j<n))
         {
-            
+            //har character ko macth kro dono string ka
             if(s.charAt(i)==t.charAt(j))
             {
                i++;
                j++;
             }
             else
+            //agr character na mile to aage badh jaye
             {
                 i++;
                 
@@ -22,7 +23,7 @@ class Solution {
              last=j;
         }
 
-        
+        //last me subsequences me jo bach gya usko string mr add krna pdega
         int  result=n-j;
         return result;
         
