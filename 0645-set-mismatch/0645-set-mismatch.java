4746449494a4457ebe3f39b682aa371cd91ba10f
satyamgiri78 <satyamgiri9569@gmail.com>
2026-08-26 01:00:20 +0530

    Time: 2 ms (94.46%), Space: 47.7 MB (34.57%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n=nums.length;
+        int[] freq=new int[n+1];
+        //frequecy of eech element
+        for(int num:nums)
+        {
+            freq[num]++;
+        }
+
+        int duplicate=-1;
+        int missing=-1;
+
+        for(int i=0;i<=n;i++)
+        {
+            if(freq[i]==2)
+            {
+                duplicate=i;
+            }
+            if(freq[i]==0)
+            {
+                missing=i;
+            }
+        }
+        return new int[]{duplicate,missing};
+    }
+}
