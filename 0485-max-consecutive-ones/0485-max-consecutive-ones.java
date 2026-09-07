1e271ba33874eb13b507d629d188c528ba77fef3
satyamgiri78 <satyamgiri9569@gmail.com>
2026-09-07 23:21:21 +0530

    Time: 2 ms (98.28%), Space: 52.6 MB (26.37%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.java
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -9,15 +9,15 @@ class Solution {
             if(nums[i]==1)
             {
                 count++;
-                max=Math.max(max,count);
+                
             }
             else
             {
-               
+               max=Math.max(max,count);
                 count=0;
             }
         }
         
-       return max;
+       return Math.max(max,count);
     }
 }
